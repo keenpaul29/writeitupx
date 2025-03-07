1f3782c4c3454c1e09dd798ec56ae449af4b974d
Puspal Paul <puspal.paul@gcettb.ac.in>
2025-03-07 20:36:58 +0530

    EDIT

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,4 @@
-import './utils/global.js';  // Fix the import path issue
+// import './utils/global.js';  // Fix the import path issue
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
