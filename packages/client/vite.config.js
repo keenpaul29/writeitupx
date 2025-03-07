2bb76b95829e6139107506b43c1feba34f4c2b76
Puspal Paul <puspal.paul@gcettb.ac.in>
2025-03-07 13:45:06 +0530

    changes again

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -17,4 +17,9 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true,
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  }
 }); 
