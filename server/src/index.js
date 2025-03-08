c0a0d5dfd3c28b5b1ccabe1ea23bd74f0cf664a6
Puspal Paul <puspal.paul@gcettb.ac.in>
2025-03-08 19:14:23 +0530

    forgot about this

--- server/src/index.js
+++ server/src/index.js
@@ -35,7 +35,7 @@ app.use(
       : ['http://localhost:3000', 'http://localhost:8000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization',"Access-Control-Allow-Origin"]
   })
 );
 
